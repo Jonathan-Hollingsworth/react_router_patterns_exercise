65120680338b246a5e0652de7b432d9c6d8b227a
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-10-12 15:16:51 -0400

    Added defaultProps for App

--- router_dog_finder/src/App.js
+++ router_dog_finder/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { Switch, Route, Redirect } from "react-router-dom"
 import './App.css';
 
-function App() {
+function App({dogs}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/dogs" >
+        <DogList dogs={dogs}/> // what props will this need?
+      </Route>
+      <Route path="/dogs/:name" >
+        <DogDetails /> // what props will this need?
+      </Route>
+      <Redirect to="/dogs" />
+    </Switch>
   );
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: duke,
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: perry,
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
 }
 
 export default App;
