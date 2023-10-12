97bbd85efc4152cd914d4f23ec2c2ecf5220ae50
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-10-12 16:48:03 -0400

    Created and exported components

---
+++ router_dog_finder/src/DogDetails.js
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom"
+
+function DogDetails({dogsByName}) {
+    const { name } = useParams()
+    const dog = dogsByName[name]
+
+    return(
+        <div className="dog">
+            <h1>{dog.name}</h1>
+            <img src={`${dog.src}`}/>
+            <b>Age {dog.age}</b>
+            <ul>
+                {dog.facts.map(fact => <li>{fact}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default DogDetails
