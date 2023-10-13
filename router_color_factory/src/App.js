3a3cbd11dd371e10335e63726bc9ac2f721a9e5d
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-10-13 15:49:38 -0400

    Created primary application

--- router_color_factory/src/App.js
+++ router_color_factory/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { Switch, Route, Redirect, Link } from "react-router-dom"
+import { v4 as uuid } from 'uuid';
+import React, { useState } from "react";
+import NewColorForm from "./NewColorForm";
+import ColorDisplay from "./ColorDisplay";
+import ColorIndex from "./ColorIndex";
 import './App.css';
 
 function App() {
+  const initialColors = []
+
+  const [colors, setColors] = useState(initialColors)
+
+  function addColor(color) {
+      let newColor = {...color, id: uuid()}
+      setColors(colors => [...colors, newColor])
+  }
+
+  const colorsByName = {}
+  for (const color of colors) {
+    colorsByName[color.name.toLowerCase()] = color
+  }
+  console.log(colorsByName)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Factory</h1>
+      <Link to="/colors/new">Add a Color</Link>
+      <br />
+      <Link to="/colors">View all Colors</Link>
+      <hr />
+      <Switch>
+        <Route exact path="/colors/new"><NewColorForm addColor={addColor}/></Route>
+        <Route exact path="/colors/:color"><ColorDisplay colorsByName={colorsByName}/></Route>
+        <Route exact path="/colors"><ColorIndex colors={colors}/></Route>
+        <Redirect to="/colors" />
+      </Switch>
     </div>
   );
 }
